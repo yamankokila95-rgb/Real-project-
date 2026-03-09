16da4dc9ba54ac78ee758d029b6eaa8ed8c4a91c
yamankokila95-rgb <yamankokila95@gmail.com>
2026-03-09 08:06:43 +0530

    Update vite.config.ts

--- CampusVoice_CodeSandbox.zip_unzipped/vite.config.ts
+++ CampusVoice_CodeSandbox.zip_unzipped/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 	  server: {
   host: true,
   port: 5173,
-  allowedHosts: "xwcc7z-5173.csb.app" 
+  allowedHosts: ['.csb.app'] 
 	  },
   build: {
     chunkSizeWarningLimit: 5000,
