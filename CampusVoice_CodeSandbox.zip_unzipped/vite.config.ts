8763d823c9fdd37002ddd22efa76c6c1b0ccac73
yamankokila95-rgb <yamankokila95@gmail.com>
2026-03-09 07:23:27 +0530

    Set allowedHosts for Vite server configuration

--- CampusVoice_CodeSandbox.zip_unzipped/vite.config.ts
+++ CampusVoice_CodeSandbox.zip_unzipped/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 	  server: {
   host: true,
   port: 5173,
-  allowedHosts: true
+  allowedHosts: "xwcc7z-5173.csb.app" 
 	  },
   build: {
     chunkSizeWarningLimit: 5000,
